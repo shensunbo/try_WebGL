305aefd85596aec9135f0f0e8d5efbc51c995006
Sunbo Shen <Sunbo.Shen@aptiv.com>
2025-08-26 17:14:54 +0800

    update halo

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ three.js
 2. build
     * npm run build
     * npm run preview
+
+# issue
+1. blender 导出GLB 文件可以正常加载，但GLB文件太大了
+![halo glb](public/res/halo.png)
+2. fbx文件无法正常加载纹理
+
--- src/js/scene.js
+++ src/js/scene.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 export class SceneManager {
     constructor(container) {
@@ -8,11 +9,13 @@ export class SceneManager {
         this.scene = new THREE.Scene();
         this.camera = new THREE.PerspectiveCamera(
             75,
-            window.innerWidth / window.innerHeight
+            window.innerWidth / window.innerHeight,
+            0.1,
+            3000
         );
-        this.camera.position.x = 0;
-        this.camera.position.y = 15;
-        this.camera.position.z = 50;
+
+        this.camera.position.set(0, 500, 30);
+        this.camera.lookAt(0, 0, 0);
         
         this.renderer = new THREE.WebGLRenderer({ antialias: true });
         this.renderer.setSize(window.innerWidth, window.innerHeight);
@@ -22,26 +25,37 @@ export class SceneManager {
 
         const ambientLight = new THREE.AmbientLight(0x404040);
         this.scene.add(ambientLight);
-        const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+        const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
         directionalLight.position.set(1, 1, 1);
         this.scene.add(directionalLight);
 
         this.controls = new OrbitControls(this.camera, this.renderer.domElement);
         this.controls.enableDamping = true;
 
-        // 创建立方体
-        // this.cube = this.createCube();
-        // this.scene.add(this.cube);
+        // const textureLoader = new THREE.TextureLoader();
+
+        // const chestMat = new THREE.MeshStandardMaterial({
+        //     map: textureLoader.load('./assets/halo/textures/Spartan_Chest_Mat_BaseColor.png'),     
+        //     normalMap: textureLoader.load('./assets/halo/textures/Spartan_Chest_Mat_Normal.png')
+        // });
+
 
         const loader = new FBXLoader();
         loader.load(
-            './assets/audi.fbx', // FBX 文件路径
+            './assets/halo/textures/halo.fbx', // FBX 文件路径
             (fbx) => {
                 console.log('FBX 模型加载成功', fbx);
 
                 // 调整模型位置/缩放（可选）
                 fbx.scale.set(0.1, 0.1, 0.1);
                 fbx.position.set(0, 0, 0);
+
+                // fbx.traverse((child) => {
+                //     if (child.isMesh && child.material.name === 'chest_Mat') {
+                //         child.material = chestMat;
+                //         child.material.needsUpdate = true;
+                //     }
+                // });
 
                 // 添加到场景ds
                 this.scene.add(fbx);
@@ -54,15 +68,15 @@ export class SceneManager {
             }
         );
 
+        // const loader = new GLTFLoader();
+        //     loader.load('./assets/halo/textures/halo.glb', (gltf) => {
+        //         this.scene.add(gltf.scene);
+        // });
+
         // 窗口大小自适应
         window.addEventListener('resize', this.onWindowResize.bind(this));
     }
 
-    createCube() {
-        const geometry = new THREE.BoxGeometry(1, 1, 1);
-        const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-        return new THREE.Mesh(geometry, material);
-    }
 
     onWindowResize() {
         this.camera.aspect = window.innerWidth / window.innerHeight;
